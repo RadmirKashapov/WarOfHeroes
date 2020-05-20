c626d418dea64ca640d17ef142a711dcf63b7de0
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2020-05-21 00:32:55 +0300

    Added ISpecialAction, IUnit, IGame interfaces and different types of units

---
+++ Assets/Scripts/Interfaces/IUnit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface IUnit
+    {
+        public int Cost { get; } // "стоимость" создания
+        public int Hp { get; set; } // оставшаяся жизнь
+        public int Ad { get; set; } // сила атаки
+        public int Df { get; set; } // уровень защиты
+        public void TakeDamage(int ad);
+    }
+}
---
+++ Assets/Scripts/Units/Infantry.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Units
+{
+    class Infantry : IUnit, ICloneable
+    {
+        public int Cost => throw new NotImplementedException();
+
+        public int Hp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Ad { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Df { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage(int ad)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
